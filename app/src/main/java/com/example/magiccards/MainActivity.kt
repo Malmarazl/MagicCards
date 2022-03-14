7581ae7590775908aa0ccf78e3e0091744051263
Malmarazl <monicalmaraz91@gmail.com>
2022-03-14 17:19:11 +0100

    Project creation, add the Main Activity and add Home Fragment

---
+++ app/src/main/java/com/example/magiccards/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.magiccards
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.view.Window
+import androidx.fragment.app.Fragment
+import com.example.magiccards.databinding.ActivityMainBinding
+import com.example.magiccards.home.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        openFragment(HomeFragment())
+    }
+
+    fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}

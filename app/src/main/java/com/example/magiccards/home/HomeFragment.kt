0e7f93d14187b11ba6b67c36e6707e4f34549245
Malmarazl <monicalmaraz91@gmail.com>
2022-03-15 11:15:59 +0100

    Add HomeFragment elements

--- app/src/main/java/com/example/magiccards/home/HomeFragment.kt
+++ app/src/main/java/com/example/magiccards/home/HomeFragment.kt
@@ -8,5 +8,11 @@ import android.view.ViewGroup
 import com.example.magiccards.R
 
 class HomeFragment : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
 }
